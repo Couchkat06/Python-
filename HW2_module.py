6c9dfdff12804279f97549d6a963fd235d5dd16a
Couchkat06 <chenkatrina06@gmail.com>
2026-05-21 23:08:32 -0400

    err idk whats going on we rolling

---
+++ HW2_module.py
@@ -0,0 +1,16 @@
+
+
+
+#Question 1
+
+def sum_of_two_numbers(num1, num2):
+    sum1 = num1 + num2
+    print(sum1)
+    return sum1
+
+def add_up_all_the_numbers(*nums):
+    sum_total = 0
+    for num in nums: 
+        sum_total += num 
+    return sum_total
+
